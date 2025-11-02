328f5b907a41b8be5f62527fc31594c754357c6f
Tabahi Dev <himankprajapati5@gmail.com>
2025-11-02 07:28:57 +0000

    conversion of datatypes

---
+++ .devcontainer/01-basics.js/03-conversionOperation.js
@@ -0,0 +1,30 @@
+let score = undefined
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber );
+console.log(valueInNumber)
+
+
+//"33" => 33
+//"33abe" => NaN
+// ture =>1 ; false => 0
+
+let isLoggedIn = "himank"
+
+let booleanIsLoggedIN = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIN);
+// 1 => true; 0 => false
+//"" => false
+//"himank" => true
+ let someNumber = 33 
+
+ let stringNumber = String(someNumber)
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
+ 
+
+
+ 
